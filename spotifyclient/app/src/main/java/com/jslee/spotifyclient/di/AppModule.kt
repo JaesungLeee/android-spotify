fcf2fd8e56d093536416122f28e7718f9fbe0ab1
Jaesung Lee <viki0114@naver.com>
2021-12-07 00:22:46 +0900

    add : Dependency Injection Setup

---
+++ spotifyclient/app/src/main/java/com/jslee/spotifyclient/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.jslee.spotifyclient.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.jslee.spotifyclient.R
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class) //Application Component Deprecated in ver 2.30
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context)
+        .setDefaultRequestOptions(
+            RequestOptions()
+                .placeholder(R.drawable.ic_image)
+                .error(R.drawable.ic_image)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+        )
+}
---
+++ spotifyclient/app/src/main/java/com/jslee/spotifyclient/di/ServiceModule.kt
@@ -0,0 +1,54 @@
+package com.jslee.spotifyclient.di
+
+
+import android.content.Context
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideAudioAttributes() = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    @ServiceScoped
+    @Provides
+    fun provideExoPlayer(
+        @ApplicationContext context: Context,
+        audioAttributes: AudioAttributes
+    ) = ExoPlayer.Builder(context).build().apply {
+        setAudioAttributes(audioAttributes, true)
+        setHandleAudioBecomingNoisy(true)
+    }
+
+    /**
+     * @since 2021.12.06
+     *
+     * @deprecated Use DefaultDataSource.Factory
+     * @param context Context
+     * @param baseDataSourceFactory DataSource.Factory
+     *
+     * DataSource.Factory에 대한 parameter 확인해야 함
+     */
+    @ServiceScoped
+    @Provides
+    fun provideDataSourceFactory(
+        @ApplicationContext context: Context
+    ) = DefaultDataSourceFactory(context, Util.getUserAgent(context, "Spotify App"))
+
+}
